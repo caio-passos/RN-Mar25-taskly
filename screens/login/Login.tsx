8b4c66fcc0b9bda18f491be158bd772507c63e9d
Caio-Passos <caiopss99@gmail.com>
2025-04-28 18:10:07 -0300

    Login done, but needs error fields

--- screens/login/Login.tsx
+++ screens/login/Login.tsx
@@ -1,12 +1,108 @@
 import React from 'react';
-import {View, Text, StyleSheet, TextInput, Button} from 'react-native';
+import { View, Text, StyleSheet, TextInput, Button, Pressable } from 'react-native';
+import Icon from '@react-native-vector-icons/ionicons';
+import Logo from '../../components/taskly.svg';
 
-function Login(){
+function Login() {
     return (
-        <View>
-        <Text>Login</Text>
+        <View style={styles.container}>
+            <View style={styles.boxLogo}>
+                <Logo width={329} height={56} />
+            </View>
+            <View style={styles.loginForm}>
+                <Text>E-mail</Text>
+                <View style={styles.boxInput}>
+                    <TextInput
+                        placeholder="Digite seu e-mail"
+                        keyboardType="email-address" />
+                </View>
+            </View>
+
+            <View style={styles.loginForm}>
+                <Text>Senha</Text>
+                <View style={styles.boxInput}>
+                    <TextInput
+                        secureTextEntry={true}
+                        placeholder="Digite sua senha"
+                        keyboardType='ascii-capable' />
+                </View>
+            </View>
+            <View style={styles.boxLembrar}>
+                <Icon name="checkbox-outline" size={20} color="#32C25B" />
+                <View style={styles.textLembrar}>
+                    <Text>Lembrar de mim</Text>
+                </View>
+            </View>
+            <Pressable style={styles.buttonFilled}>
+                <Text style={styles.textEntrar}>Entrar</Text>
+            </Pressable>
+            <Pressable style={styles.buttonEmptyFill}>
+                <Text style={styles.textCriarConta}>Criar conta</Text>
+            </Pressable>
+
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingHorizontal: 80,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    boxLogo:{
+        justifyContent: 'center',
+        paddingBottom: 24,
+    },
+    loginForm: {
+        width: '100%',
+        marginBottom: 20,
+        gap: 4,
+    },
+    boxInput: {
+        width: '100%',
+        height: 47,
+        borderWidth: 2,
+        borderColor: '#5B3CC4',
+        borderRadius: 8,
+        justifyContent: 'center',
+    },
+    boxLembrar: {
+        flexDirection: 'row',
+        alignSelf: 'flex-start',
+    },
+    textLembrar: {
+        alignSelf: 'flex-start',
+        paddingHorizontal: 4,
+    },
+    buttonFilled: {
+        backgroundColor: '#5B3CC4',
+        borderRadius: 8,
+        width: '100%',
+        height: 47,
+        justifyContent: 'center',
+        marginTop: 25,
+    },
+    textEntrar: {
+        color: '#FFFFFF',
+        textAlign: 'center',
+    },
+    buttonEmptyFill: {
+        borderColor: '#5B3CC4',
+        borderWidth: 2,
+        borderRadius: 8,
+        width: '100%',
+        height: 47,
+        justifyContent: 'center',
+        marginTop: 25,
+    },
+    textCriarConta: {
+        color: '#5B3CC4',
+        textAlign: 'center',
+    },
+
+});
 
 export default Login;
