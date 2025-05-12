c51b75a600bff26cca7a16a98832ca4c73b5a20b
Douglas Bastos <dautodouglasdauto@gmail.com>
2025-05-12 00:17:48 -0300

    fix: Restore Validation Create Account

--- screens/Cadastro.tsx
+++ screens/Cadastro.tsx
@@ -14,6 +14,7 @@ import { mmkvStorage } from '../services/db/storageMMKV';
 interface CadastroProps extends NativeStackScreenProps<RootStackParamList, 'Cadastro'> {}
 
 type dataUser = { uid: string, nome: string, email: string, telefone: string, senha: string, checkSenha?: string }
+type DataInfoUser = { nameAndSurname: string, email: string, numberPhone: string, password: string, confirmPassword: string }
 
 function Cadastro({ navigation }: CadastroProps) {
     const { colors, darkMode } = useContext(AppContext)!;
@@ -26,6 +27,11 @@ function Cadastro({ navigation }: CadastroProps) {
     const [telefone, setTelefone] = useState('');
     const [senha, setSenha] = useState('');
     const [checkSenha, setCheckSenha] = useState('');
+
+    const [nameAndSurname, setNameAndSurname] = useState('');
+    const [numberPhone, setNumberPhone] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
 
     const [errorsNome, setErrorsNameAndSurname] = useState(error);
     const [errorsEmail, setErrorsEmail] = useState(error);
@@ -73,6 +79,15 @@ function Cadastro({ navigation }: CadastroProps) {
             navigation.navigate('Dashboard');
         }
     }
+
+    useEffect(() => {
+        setNameAndSurname(nome);
+        setNumberPhone(telefone);
+        setPassword(senha);
+        setConfirmPassword(checkSenha);
+        setEmailValidation(email);
+    }, [nome, telefone, senha, checkSenha, email]);
+
     useEffect(() => {
         const verifyPasswords = () => {
             if (checkSenha === '') {
@@ -209,8 +224,8 @@ function Cadastro({ navigation }: CadastroProps) {
         },
     });
 
-    async function createAccount(data: dataUser) {
-        const { isValid, errors } = verifyData(data);
+    async function createAccount(data: dataUser, dataVerifiy: DataInfoUser) {
+        const { isValid, errors } = verifyData(dataVerifiy);
 
         if (!isValid) {
             showErrors(isValid, errors);
@@ -218,11 +233,101 @@ function Cadastro({ navigation }: CadastroProps) {
             hideErrors();
             setItemUserData({ ...data, loggedIn: true });
         }
+
+        return isValid;
     }
 
-    function verifyData(data: dataUser) {
-        console.log("DEBUG: Skipping validation, assuming valid.");
-        return { isValid: true, errors: [] };
+    function verifyData(data: DataInfoUser) {
+        const regexEmail: RegExp = /^[\w.-]+@[\w.-]+\.\w{2,}$/;
+        const errors: Array<{ tag: string, error: string }> = [];
+        const regexPasswordSpecial = /[^a-zA-Z0-9]/;
+        const regexPasswordUpper = /[A-Z]/;
+        const regexPasswordLower = /[a-z]/;
+
+        function verifyName() {
+            const name = data.nameAndSurname.split(' ');
+            const errors = { notSurname: '', notLength: '' };
+
+            if (name.length < 2 || name[1] === '') {
+                errors.notSurname = 'notSurname';
+            }
+
+            if (name.join('').length > 120) {
+                errors.notLength = 'notLength';
+            }
+
+            return errors;
+        }
+
+        function verifyPhone() {
+            const errors = { notLength: '' };
+            const lenghtPhone = data.numberPhone.replace('(', '').replace(')', '').replace('-', '').replace(' ', '').length;
+
+            if (lenghtPhone < 10 || lenghtPhone < 11) {
+                errors.notLength = 'notLength';
+            }
+
+            return errors;
+        }
+
+        function isSequence(text: string) {
+            const chars: Array<string> = [];
+            const numberSequence: Array<string> = text.split('');
+            const textOrder: string = numberSequence.sort().join('');
+
+            for (let i: number = 0; i < text.length; i++) {
+                if (text[i + 1] !== text[i] && i < text.length && !/\d/.test(text[i])) {
+                    chars.push(text[i]);
+                } else if (textOrder[i + 1] !== textOrder[i] && Number(textOrder[i + 1]) - 1 !== (Number(textOrder[i])) && i < textOrder.length) {
+                    chars.push(text[i]);
+                }
+            }
+
+            return chars.length === text.length;
+        }
+
+        if (verifyName().notSurname === 'notSurname') {
+            errors.push({ tag: 'name', error: 'O nome deve ser composto (mínimo dois nomes)' });
+        }
+
+        if (verifyName().notLength === 'notLength') {
+            errors.push({ tag: 'name', error: 'Máximo de 120 caracteres' });
+        }
+
+        if (!regexEmail.test(data.email)) {
+            errors.push({ tag: 'email', error: 'Email inválido' });
+        }
+
+        if (verifyPhone().notLength === 'notLength') {
+            errors.push({ tag: 'phone', error: 'O número deve ter 11 ou 12 digitos' });
+        }
+
+        if (data.password.length < 8) {
+            errors.push({ tag: 'password', error: 'A senha deve ter no mínimo 8 caracteres' });
+        } else if (data.password.length >= 8 && data.password.length > 20) {
+            errors.push({ tag: 'password', error: 'A senha deve ter no máximo 20 caracteres' });
+        } else if (data.password !== '' && !isSequence(data.password)) {
+            errors.push({ tag: 'password', error: 'A senha não pode ser uma sequência igual' });
+        } else if (!regexPasswordUpper.test(data.password)) {
+            errors.push({ tag: 'password', error: 'A senha deve ter uma letra maiúscula' });
+        } else if (!regexPasswordLower.test(data.password)) {
+            errors.push({ tag: 'password', error: 'A senha deve ter uma letra minúscula' });
+        } else if (!regexPasswordSpecial.test(data.password)) {
+            errors.push({ tag: 'password', error: 'A senha deve ter um caractere especial' });
+        }
+
+        if (data.confirmPassword !== data.password || data.confirmPassword === '') {
+            errors.push({ tag: 'passwordConfirm', error: 'A senhas devem ser iguais' });
+        }
+
+        const isValid = errors.length <= 0;
+
+        return {
+            isValid,
+            errors,
+        };
+        //console.log("DEBUG: Skipping validation, assuming valid.");
+        //return { isValid: true, errors: [] };
     }
 
     function showErrors(isValid: boolean, errors: Array<{ tag: string, error: string }>) {
@@ -230,8 +335,8 @@ function Cadastro({ navigation }: CadastroProps) {
             const errorsNome = errors.filter((value) => value.tag === 'name');
             const errorsEmail = errors.filter((value) => value.tag === 'email');
             const errorsTelefone = errors.filter((value) => value.tag === 'phone');
-            const errorssenha = errors.filter((value) => value.tag === 'senha');
-            const errorssenhaConfirm = errors.filter((value) => value.tag === 'senhaConfirm');
+            const errorssenha = errors.filter((value) => value.tag === 'password');
+            const errorssenhaConfirm = errors.filter((value) => value.tag === 'passwordConfirm');
 
             setErrorsNameAndSurname(errorsNome);
             setErrorsEmail(errorsEmail);
@@ -372,13 +477,19 @@ function Cadastro({ navigation }: CadastroProps) {
             <Pressable
                 style={styles.buttonFilled}
                 onPress={() => {
-                    if (isFilled && !senhaError) {
-                        createAccount(formData);
-                        handleOpenModal();
-                        //add api post to-do
-                    } else {
-                        console.log("Submit blocked: isFilled=", isFilled, "senhaError=", senhaError);
+                    //if (isFilled && !senhaError) {
+                    let { isValid } = verifyData({ nameAndSurname: nameAndSurname, email: emailValidation, numberPhone: numberPhone, password: password, confirmPassword: confirmPassword });
+                    createAccount(formData, { nameAndSurname: nameAndSurname, email: emailValidation, numberPhone: numberPhone, password: password, confirmPassword: confirmPassword });
+
+                    if (!isValid) {
+                        return;
                     }
+
+                    handleOpenModal();
+                        //add api post to-do
+                    //} else {
+                    //    console.log("Submit blocked: isFilled=", isFilled, "senhaError=", senhaError);
+                    //}
                 }}>
                 <Text style={styles.textCriarConta}>CRIAR CONTA</Text>
             </Pressable>
